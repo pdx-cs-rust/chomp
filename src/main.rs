5d54c219a11654635a5684bd0cb8c88184091cfd
Bart Massey <bart@cs.pdx.edu>
2018-08-15 13:57:08 -0700

    added display trait for bar

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@
 //! Chomp player in Rust.
 
 use std::collections::HashSet;
+use std::fmt::{self, Display};
 
 /// Structure representing a candy bar to be chomped.
 struct Bar(HashSet<(u64, u64)>);
     
 impl Bar {
+    /// Create a new `Bar` with the given `width` and `height`.
     fn new(width: u64, height: u64) -> Bar {
         let mut bar = HashSet::with_capacity((width * height) as usize);
         for row in 0..height {
@@ -18,6 +20,7 @@ impl Bar {
         Bar(bar)
     }
 
+    /// True iff the given `row` / `col` is occupied.
     fn contains(&self, row: u64, col: u64) -> bool {
         self.0.contains(&(row, col))
     }
@@ -30,6 +33,25 @@ fn new_bar_ok() {
     assert!(b.contains(4, 3));
     assert!(!b.contains(5, 4));
     assert!(b.0.len() == 20);
+}
+
+impl Display for Bar {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for row in 0.. {
+            if !self.contains(row, 0) {
+                break;
+            }
+            for col in 0.. {
+                if self.contains(row, col) {
+                    write!(f, "*")?;
+                } else {
+                    break;
+                }
+            }
+            write!(f, "\n")?;
+        }
+        Ok(())
+    }
 }
 
 fn main() {
